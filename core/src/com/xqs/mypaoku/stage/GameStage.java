68a2bc90b66d3ed502a78aeb0aaf7cae79ab587d
chanson1024 <lovezww2011@gmail.com>
2017-03-15 21:59:14 +0800

    Update some code

--- core/src/com/xqs/mypaoku/stage/GameStage.java
+++ core/src/com/xqs/mypaoku/stage/GameStage.java
@@ -210,6 +210,7 @@ public class GameStage extends BaseStage {
 						case Bullet.PLAYER:
 							//此子弹会自爆
 							if(bullet.getState()==Bullet.EXPLODE) {
+								Util.log(TAG,"enemy====="+enemy.getState());
 								enemy.hurt();
 							}
 							break;
